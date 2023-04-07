d4b0e9835a4715409d5b0207c88bc33e5e3ebac8
BANDDIT <arrickrussellg@gmail.com>
2023-04-07 22:08:43 +0700

    feat : adding create tips

--- Create/Create.js
+++ Create/Create.js
@@ -16,36 +16,7 @@ let offer_copy = document.querySelector('.caption-content');
 
 let attachment = document.getElementById('attachment');
 
-//Menu Pitch dan Tips
-/*
-pitch_menu.addEventListener('click',function(e){
-    //alert("PITCH DIKLIK");
-    pitch_menu.style.color= "#203D3B";
-    pitch_menu.style.textDecoration = "underline";
-    tips_menu.style.color= "#447270";
-    tips_menu.style.textDecoration = "none";
 
-    pitch_page.style.display="flex";
-    pitch_page.style.pointerEvents="auto";
-    tips_page.style.display="none";
-    tips_page.style.pointerEvents="none";
-});
-tips_menu.addEventListener('click',function(e){
-    //alert("TIPS DIKLIK");
-    pitch_menu.style.color= "#447270";
-    pitch_menu.style.textDecoration = "none";
-    tips_menu.style.color= "#203D3B";
-    tips_menu.style.textDecoration = "underline";
-
-    pitch_page.style.display="none";
-    pitch_page.style.pointerEvents="none";
-    tips_page.style.display="flex";
-    tips_page.style.pointerEvents="auto";
-});
-*/
-
-//Pitch 
-//Copas input ke preview
 fullname_input.addEventListener('input',function(e){
     fullname_copy.innerText = fullname_input.value;
 });
@@ -53,76 +24,15 @@ bisnisName_input.addEventListener('input',function(e){
     bisnisName_copy.innerText= bisnisName_input.value;
 });
 firstCateg_input.addEventListener('input',function(e){
-    //alert("KLoK");
     firstCateg_copy.innerText = firstCateg_input.value;
-    //firstCateg_copy.innerText = firstCateg_input.value;
 });
 secCateg_input.addEventListener('input',function(e){
     secCateg_copy.innerText = secCateg_input.value;
 });
-/*
-generalDesc_input.addEventListener('input',function(e){
-    generalDesc_copy.innerText = generalDesc_input.value;
-});*/
 offer_input.addEventListener('input',function(e){
     offer_copy.innerText = offer_input.value;
-    //offer_copy.
 });
-/*
-$(document).ready(function(){
-    let emojioneArea = $('#txt-offerdetails').emojioneArea({
-        pickerPosition: 'bottom',
-        search: true,
-        filters: {
-            shortnames: false,
-            emoticons: false,
-            custom: []
-        },
-        events: {
-            keyup: function (editor, event) {
-                offer_copy.innerHTML = emojioneArea[0].emojioneArea.getText();
-            },
-            emojibtn_click: function (button, event) {
-                offer_copy.innerHTML = emojioneArea[0].emojioneArea.getText();
-            },
-        },
-    });
-    $('.input-generaldesc').on('input',function(){
-        generalDesc_copy.innerText = generalDesc_input.value;
-    });
-});*/
-/*
-$(document).ready(function(){
-    let emojioneArea = $('#txt-offerdetails').emojioneArea({
-        pickerPosition: 'bottom',
-        search: true,
-        filters: {
-            shortnames: false,
-            emoticons: false,
-            custom: []
-        },
-        events: {
-            keyup: function (editor, event) {
-                if (event.key === 'Enter') {
-                    //offer_copy.innerHTML += '<br>';
-                    editor.insert('\n');
-                }
-                else{
-                    offer_copy.innerHTML = emojioneArea[0].emojioneArea.getText();
-                }
-            },
-            emojibtn_click: function (button, event) {
-                offer_copy.innerHTML = emojioneArea[0].emojioneArea.getText();
-            },
-        },
-    });
 
-    $('#txt-offerdetails').on('keypress', function(event) {
-        if (event.key === 'Enter') {
-            offer_copy.innerHTML += '<br>';
-        }
-    });
-});*/
 
 $(document).ready(function(){
     let emojioneArea = $('#txt-offerdetails').emojioneArea({
@@ -155,22 +65,15 @@ $(document).ready(function(){
 });
 
 
-  
-
-
 let imageUpload = document.getElementById('picture-general');
-//let imagePreview = document.querySelector('.banner');
 let close_banner = document.querySelector('.banner img');
 let banner_div = document.querySelector('.banner-div');
 imageUpload.addEventListener('change',function(){
 	let file = this.files[0];
-    //imagePreview.style.display="flex";
     if(file){
         let reader = new FileReader();
-
+		reader.readAsDataURL(file);        
 		reader.addEventListener('load', function() {
-			//imagePreview.innerHTML = '<img src="' + this.result + '" />';
-            //alert("TESTETS");
             let elemen = document.createElement('div');
             elemen.classList.add('banner');
             elemen.style.backgroundImage= "url('" + reader.result + "')";
@@ -181,62 +84,35 @@ imageUpload.addEventListener('change',function(){
 
             elemen.appendChild(pict);
             banner_div.appendChild(elemen);
-            /*
-            imagePreview.style.backgroundImage = "url('" + reader.result + "')";
-            close_banner.style.display="flex";*/
 		});
-
-		reader.readAsDataURL(file);        
     }
 });
 
-/*
-imageUpload.addEventListener('change', function() {
-	let file = this.files[0];
-    imagePreview.style.display="flex";
-	//alert("ASDJASOIJDAS");
-	if (file) {
-		const reader = new FileReader();
-
-		reader.addEventListener('load', function() {
-			//imagePreview.innerHTML = '<img src="' + this.result + '" />';
-            imagePreview.style.backgroundImage = "url('" + reader.result + "')";
-            close_banner.style.display="flex";
-		});
-
-		reader.readAsDataURL(file);
-	}
-});
-
-close_banner.addEventListener('click',function(e){
-    close_banner.style.display="none";
-    imagePreview.style.display="none";
-
-});*/
 
 
 let pitch_preview = document.querySelector('.pitch-preview-content');
 let close_link = document.querySelector('.link-box-close');
 attachment.addEventListener('change',function(){
-    //alert("SADASD");
     let elemen = document.createElement('div');
     elemen.classList.add('link-box');
-
     let input = document.getElementById("attachment");
     let file = input.files[0];
+
+    if(input.files[1]!=null){
+        file = input.files[1];
+        console.log(file);
+    }
+
     let url = URL.createObjectURL(file);
     let link = document.createElement("a");
     link.setAttribute("href", url);
     link.setAttribute("download", file.name);
     link.innerHTML = file.name;
-    elemen.appendChild(link);
-
-    
+    elemen.appendChild(link); 
     let pict = document.createElement('img');
     pict.classList.add('link-box-close');
     pict.src="../Asset/close.png";
     elemen.appendChild(pict);
-
     pitch_preview.appendChild(elemen);
 });
 
@@ -246,139 +122,33 @@ pitch_preview.addEventListener('click', function(e) {
     if (obj.classList.contains('link-box-close')||obj.classList.contains('close_banner')) {
       obj.parentElement.remove();
     }
-
 });
 
 
-
-/*
-let pitch_menu = document.querySelector('.sub-header-pitch');
-let tips_menu = document.querySelector('.sub-header-tips');
-let pitch_page = document.querySelector('.pitch-page');
-let tips_page = document.querySelector('.tips-page');
-
-let fullname_copy = document.querySelector('.head-pitch-preview-content-judul-h2-1');
-let bisnisName_copy = document.querySelector('.head-pitch-preview-content-judul-h2-2');
-let fullname_input = document.querySelector('.input-fullname');
-let bisnisName_input = document.querySelector('.input-businessproduct');
-
-let firstCateg_input = document.querySelector('.first-category');
-let secCateg_input = document.querySelector('.second-category');
-let firstCateg_copy = document.querySelector('.first-category-container');
-let secCateg_copy = document.querySelector('.second-category-container');
-
-let generalDesc_input = document.querySelector('.input-generaldesc');
-let generalDesc_copy = document.querySelector('.opening-content');
-
-let offer_input = document.querySelector('.input-offerdetails');
-let offer_copy = document.querySelector('.caption-content');
-
-let attachment = document.getElementById('attachment');
-
-let bottom_caps_input = document.querySelector('.input-caption');
-let bottom_caps_copy = document.querySelector('.preview-bottom-caption');
-
-let tags_input = document.querySelector('.input-tag');
-let tags_copy = document.querySelector('.tags');
-
-
-let attachment_tips = document.querySelector('.attachment-tips');
-let post_picture= document.querySelector('.tips-preview-content-image');
-
-//Menu Pitch dan Tips
-pitch_menu.addEventListener('click',function(e){
-    //alert("PITCH DIKLIK");
-    pitch_menu.style.color= "#203D3B";
-    pitch_menu.style.textDecoration = "underline";
-    tips_menu.style.color= "#447270";
-    tips_menu.style.textDecoration = "none";
-
-    pitch_page.style.display="flex";
-    pitch_page.style.pointerEvents="auto";
-    tips_page.style.display="none";
-    tips_page.style.pointerEvents="none";
-});
-tips_menu.addEventListener('click',function(e){
-    //alert("TIPS DIKLIK");
-    pitch_menu.style.color= "#447270";
-    pitch_menu.style.textDecoration = "none";
-    tips_menu.style.color= "#203D3B";
-    tips_menu.style.textDecoration = "underline";
-
-    pitch_page.style.display="none";
-    pitch_page.style.pointerEvents="none";
-    tips_page.style.display="flex";
-    tips_page.style.pointerEvents="auto";
-});
-
-
-//Pitch 
-//Copas input ke preview
-fullname_input.addEventListener('input',function(e){
-    fullname_copy.innerText = fullname_input.value;
-});
-bisnisName_input.addEventListener('input',function(e){
-    bisnisName_copy.innerText= bisnisName_input.value;
-});
-firstCateg_input.addEventListener('input',function(e){
-    //alert("KLoK");
-    firstCateg_copy.innerText = firstCateg_input.value;
-    //firstCateg_copy.innerText = firstCateg_input.value;
-});
-secCateg_input.addEventListener('input',function(e){
-    secCateg_copy.innerText = secCateg_input.value;
-});
-generalDesc_input.addEventListener('input',function(e){
-    generalDesc_copy.innerText = generalDesc_input.value;
-});
-offer_input.addEventListener('input',function(e){
-    offer_copy.innerText = offer_input.value;
-    //offer_copy.
-});
-bottom_caps_input.addEventListener('input',function(e){
-    //alert("TESTER");
-    bottom_caps_copy.innerText= bottom_caps_input.value;
-});
-tags_input.addEventListener('input',function(e){
-    //alert("ASSDASDSA");
-    let string = tags_input.value;
-    if(string.charAt(0) != '@') {
-        string = '@' + string;
+let create_post = document.querySelector('.button-create-post-btn');
+let error = document.getElementById('errorMessage');
+create_post.addEventListener('click',function(e){
+    if(fullname_input.value.trim()==""){
+        e.preventDefault();
+        error.innerText="Full Name is Required";
     }
-    string = string.replace(/ /g,' @');
-    string = string.replace(/\n/g,'\n@');
-    tags_copy.innerText = string;
-    // tags_copy.innerText = tags_input.value;
-    // if(tags_input.value.charAt(tags_input.value.length()-1).equals(" ")){
-    //     tags_copy.innerText = tags_input.value + '@';
-    // }
+    else if(bisnisName_input.value.trim()==""){
+        e.preventDefault();
+        error.innerText="Full Name is Required";
+    }
+    else if(firstCateg_input.value.trim()==""){
+        e.preventDefault();
+        error.innerText="Business Type 1 is Required";
+    }
+    else if(secCateg_input.value.trim()==""){
+        e.preventDefault();
+        error.innerText="Business Type 2 is Required";
+    }
+    else if(offer_input.value.trim()==""){
+        e.preventDefault();
+        error.innerText="Caption is Required";
+    }
+    else{
+        error.innerText="";
+    }
 });
-
-
-attachment_tips.addEventListener('change',function(e){
-    let reader = new FileReader();
-    reader.readAsDataURL(attachment_tips.files[0]);
-    reader.addEventListener('load',function(e){
-        post_picture.innerHTML = "<img src=${reader.result} alt=''/>";
-    });
-});
-*/
-/*
-let imageInput = document.getElementById('.attachment-tips');
-let imagePreview = document.getElementById('.tips-preview-content-image');
-
-imageInput.addEventListener('change', function() {
-  const file = this.files[0];
-  
-  if (file) {
-    const reader = new FileReader();
-    reader.addEventListener('load', function() {
-      const image = new Image();
-      image.src = reader.result;
-      imagePreview.innerHTML = '';
-      imagePreview.appendChild(image);
-    });
-    reader.readAsDataURL(file);
-  }
-});
-*/
--- Create/Create_Tips.js
+++ Create/Create_Tips.js
@@ -2,22 +2,13 @@ let pitch_menu = document.querySelector('.sub-header-pitch');
 let tips_menu = document.querySelector('.sub-header-tips');
 let tips_page = document.querySelector('.tips-page');
 
-
-
-let bottom_caps_input = document.querySelector('.input-caption');
-let bottom_caps_copy = document.querySelector('.preview-bottom-caption');
-
 let tags_input = document.querySelector('.input-tag');
 let tags_copy = document.querySelector('.tags');
-
 
 let attachment_tips = document.querySelector('.attachment-tips');
 let post_picture= document.querySelector('.tips-preview-content-image');
 
-bottom_caps_input.addEventListener('input',function(e){
-    //alert("TESTER");
-    bottom_caps_copy.innerText= bottom_caps_input.value;
-});
+
 tags_input.addEventListener('input',function(e){
     //alert("ASSDASDSA");
     let string = tags_input.value;
@@ -26,20 +17,132 @@ tags_input.addEventListener('input',function(e){
     }
     string = string.replace(/ /g,' @');
     string = string.replace(/\n/g,'\n@');
+    //string = string.replace(/.@/g,'@'); 
     tags_copy.innerText = string;
-    // tags_copy.innerText = tags_input.value;
-    // if(tags_input.value.charAt(tags_input.value.length()-1).equals(" ")){
-    //     tags_copy.innerText = tags_input.value + '@';
-    // }
 });
 
+
+//let bottom_caps_input = document.querySelector('.input-caption');
+let bottom_caps_copy = document.querySelector('.preview-bottom-caption');
 $(document).ready(function(){
-  $('#txt-caption').emojioneArea({
-      pickerPosition:'top'
+  let emojioneArea = $('#txt-caption').emojioneArea({
+      rows:50,
+      pickerPosition: 'bottom',
+      search: false,
+      filters: {
+          shortnames: false,
+          emoticons: false,
+          custom: []
+      },
+      events: {
+          keyup: function (editor, event) {
+            
+              let texts = emojioneArea[0].emojioneArea.getText();
+              texts = texts.replace(/\n/g,"<br>");
+              if (event.key === 'Enter') {
+                  bottom_caps_copy.innerHTML += '<br>';
+                  event.preventDefault();
+              } else {
+                  bottom_caps_copy.innerHTML = texts;
+              }
+          },
+          emojibtn_click: function (button, event) {
+              let texts = emojioneArea[0].emojioneArea.getText();
+              texts = texts.replace(/\n/g,"<br>");
+              bottom_caps_copy.innerHTML = texts;
+
+          },
+      },
   });
-
-
 });
+
+
+let heart = document.getElementById('heart-btn');
+heart.addEventListener('click',function(){
+    if (heart.src.endsWith("noLike.png")) {
+        heart.src = "../Asset/heart-Like.png";
+    } else {
+        heart.src = "../Asset/heart-noLike.png";
+    }
+});
+
+
+let image_prev = document.querySelector('.tips-preview-content-image-pict-frame');
+let input_img = document.getElementById('picture-general');
+input_img.addEventListener('change',function(){
+	let file = input_img.files[0];
+    if(file){
+        let reader = new FileReader();
+		reader.readAsDataURL(file);    
+		reader.addEventListener('load', function() {
+            let elemen = document.createElement('div');
+            elemen.classList.add("slider-image-frame");
+            let pict = document.createElement('img');
+            pict.src=reader.result;
+            elemen.appendChild(pict);
+            image_prev.appendChild(elemen);
+		});
+		reader.readAsDataURL(file);        
+    }
+});
+
+
+let sub_frame = document.querySelector('.tips-preview-content-image-pict-frame');
+let prev = document.querySelector('.prev');
+let next = document.querySelector('.next');
+let index = 0; // variabel untuk menyimpan index gambar
+next.addEventListener('click',function(){
+  if (index < sub_frame.children.length - 1) { // cek apakah index masih kurang dari jumlah gambar dikurangi satu
+    index++; // jika iya, index ditambah satu
+    sub_frame.style.transform = 'translateX(' + (-index * 100) + '%)'; // posisi gambar digeser sejauh satu gambar ke kanan
+  }
+});
+prev.addEventListener('click',function(){
+  if (index > 0) { // cek apakah index masih lebih besar dari 0
+    index--; // jika iya, index dikurangi satu
+    sub_frame.style.transform = 'translateX(' + (-index * 100) + '%)'; // posisi gambar digeser sejauh satu gambar ke kiri
+  }
+});
+
+/*
+let currentSlide = 0;
+const slides = document.querySelectorAll('.tips-preview-content-image img');
+const numSlides = slides.length;
+const sliderFrame = document.querySelector('.tips-preview-content-image-pict-frame');
+let next = document.querySelector('.next');
+let prev = document.querySelector('.prev');
+next.addEventListener('click', function(e) {
+  e.preventDefault();
+  currentSlide++;
+  if (currentSlide >= numSlides) {
+    currentSlide = 0;
+  }
+  sliderFrame.style.transform = 'translateX(${currentSlide * width_frame}px)';
+});
+
+prev.addEventListener('click', function(e) {
+  e.preventDefault();
+  currentSlide--;
+  if (currentSlide < 0) {
+    currentSlide = numSlides - 1;
+  }
+  sliderFrame.style.transform = 'translateX(-${currentSlide * width_frame}px)';
+});
+*/
+
+/*
+let next = document.querySelector('.next');
+let prev = document.querySelector('.prev');
+let slider_frame = document.querySelector('.tips-preview-content-image-pict');
+next.addEventListener('click',function(e){
+    e.preventDefault();
+    slider_frame.style.transform='translateX(200%)';
+});
+prev.addEventListener('click',function(e){
+    e.preventDefault();
+    slider_frame.style.transform='translateX(-200%)';
+});
+*/
 
 /*
 attachment_tips.addEventListener('change',function(e){
@@ -47,7 +150,7 @@ attachment_tips.addEventListener('change',function(e){
     reader.readAsDataURL(attachment_tips.files[0]);
     reader.addEventListener('load',function(e){
         post_picture.innerHTML = "<img src=${reader.result} alt=''/>";
-    });
+    }); 
 });
 */
 /*
